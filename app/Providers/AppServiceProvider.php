2ad2294381b7d9b38a3aa694e32ecab3adfde235
gulni-hal <gulni.eruslu@gmail.com>
2026-05-26 01:20:15 +0300

    css lerin gozukmesi icin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // BU SATIRI EKLE
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // BU KOD BLOGUNU EKLE
+        // Sadece canlı sunucuda (production) HTTPS'e zorlar
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
